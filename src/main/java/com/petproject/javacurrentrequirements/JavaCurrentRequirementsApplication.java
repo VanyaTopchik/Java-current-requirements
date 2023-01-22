cb906da9ef6ef0dd73dff2ec6c46be6270421f59
VanyaTopchik <gvanya97@mail.ru>
2023-01-22 10:51:47 +0300

    Init project

---
+++ src/main/java/com/petproject/javacurrentrequirements/JavaCurrentRequirementsApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.javacurrentrequirements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCurrentRequirementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCurrentRequirementsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petproject/javacurrentrequirements/JavaCurrentRequirementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.javacurrentrequirements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCurrentRequirementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
